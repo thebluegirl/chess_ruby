9b02c13e1542035f01c8db6cd141751c29bfcf29
Funto <funto.afuwape@gmail.com>
2023-12-06 11:59:43 +0100

    Create Pawn#find_possible_moves
    Pawn#move_piece was also renamed to Pawn#change_location.

--- lib/pieces/pawns.rb
+++ lib/pieces/pawns.rb
@@ -5,11 +5,27 @@ class Pawn
     @history = []
   end
 
-  def move_peice(new_location)
+  def change_location(new_location)
     @history << @current_location
     @current_location = new_location
   end
 
+  def find_possible_moves
+    possible_moves_array = []
+    square_ahead = lambda do |square|
+      next_square = ''
+      next_square << square.chars[0]
+      next_square << (square.chars[1].to_i + 1).to_s
+      next_square
+    end
+    if @history.empty?
+      possible_moves_array << square_ahead.call(@current_location)
+      possible_moves_array << square_ahead.call(possible_moves_array[0])
+    else
+      possible_moves_array << square_ahead.call(@current_location)
+    end
+    possible_moves_array
+  end
   # protected
 
   attr_accessor :history, :current_location

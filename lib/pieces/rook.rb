f7f9b3b70b81c561a6f29a5a9a532639571507ec
Funto <funto.afuwape@gmail.com>
2023-12-06 13:18:46 +0100

    Create Rook#change_location

--- lib/pieces/rook.rb
+++ lib/pieces/rook.rb
@@ -1,5 +1,16 @@
 class Rook
   def initialize(symbol, default_location)
-
+    @symbol = symbol
+    @current_location = default_location
+    @history = []
   end
+
+  def change_location(new_location)
+    @history << @current_location
+    @current_location = new_location
+  end
+
+  # protected
+
+  attr_accessor :current_location, :history
 end
--- spec/pieces/rook_spec.rb
+++ spec/pieces/rook_spec.rb
@@ -1,5 +1,19 @@
 require './lib/pieces/rook'
 
 describe Rook do
-  
+  subject(:rook) { described_class.new('♖', 'a1') }
+  describe '#change_location' do
+    context 'when method is run with a valid argument provided' do
+      it 'changes the location of the piece' do
+        new_location = 'a5'
+        expect{ rook.change_location(new_location) }.to change{ rook.current_location }.from('a1').to('a5')
+      end
+
+      it 'adds the previous location to the peice history' do
+        new_location = 'a5'
+        rook.change_location(new_location)
+        expect(rook.history.include?('a1')).to be true
+      end
+    end
+  end
 end
